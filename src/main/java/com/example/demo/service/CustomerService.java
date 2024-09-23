a1732ea507da18c636126f6af16febd2e9ec456e
Leonardo Zamariola <leonardo.zamariola@gmail.com>
2024-09-23 12:12:00 -0300

    Adding try-catch

--- src/main/java/com/example/demo/service/CustomerService.java
+++ src/main/java/com/example/demo/service/CustomerService.java
@@ -3,6 +3,7 @@ package com.example.demo.service;
 import com.example.demo.database.CustomerEntity;
 import com.example.demo.database.CustomerEntityRepository;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,18 +11,23 @@ import java.util.List;
 
 @Component
 @AllArgsConstructor
+@Slf4j
 public class CustomerService {
 
     private final CustomerEntityRepository repository;
 
     private final CustomerTotalService customerTotalService;
 
-    //TODO: Add transactional to avoid partial updates
     @Transactional
     public CustomerEntity save(CustomerEntity customer) {
         final var entity = this.repository.save(customer);
 
-        this.customerTotalService.calculateTotal();
+        //TODO: add try catch to avoid stopping the transaction
+        try {
+            this.customerTotalService.calculateTotal();
+        } catch (Exception ex) {
+            log.error("Error while calculating totals", ex);
+        }
 
         return entity;
     }
